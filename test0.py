fef6a38942519b1bb1891da86ebc7bf832732d2c
Steve Lerner <slerner@splunk.com>
2021-02-01 11:41:30 -0500

    commit

--- test0.py
+++ test0.py
@@ -1,4 +1,17 @@
 import os, time
+
+class color:
+   PURPLE = '\033[1;35;48m'
+   CYAN = '\033[1;36;48m'
+   BOLD = '\033[1;37;48m'
+   BLUE = '\033[1;34;48m'
+   GREEN = '\033[1;32;48m'
+   YELLOW = '\033[1;33;48m'
+   RED = '\033[1;31;48m'
+   BLACK = '\033[1;30;48m'
+   UNDERLINE = '\033[4;37;48m'
+   END = '\033[1;37;0m'
+
 grid=[]
 grid=[[' '] * 10 for x in range(0,10)]
 #grid=[[x] * 10 for x in range(0,10)]
@@ -7,12 +20,6 @@ alienflip=0
 alienY=0
 alienX=3
 missileX=3
-
-def printgrid():
-    for y in range(0,9):
-        for x in range(0,9):
-            print(grid[y][x],end='')
-#       print('\r')
 
 def alienflipper():
         if alienflip==0:
@@ -29,14 +36,13 @@ def clearscreen(sleeptime):
 def printgrid():
     for y in range(10):
         for x in range(10):
-            print(grid[y][x],end='')
+            print(color.RED,grid[y][x],color.END,end='')
         print('\r')
 
 def mainroutine():
     for m in range(9,-1,-1):
-        grid[m][3]='^'
+        grid[m][3]=('^')
         printgrid()
-#       print('\r',m)
 #       input(" Press Enter to continue...")
         if m > 0:
                 clearscreen(.250)
