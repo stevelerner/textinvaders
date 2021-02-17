9de2c783eabd2498ce7c7bbd575372e671b17a29
Steve Lerner <slerner@splunk.com>
2021-02-16 22:41:39 -0500

    commit

--- test.py
+++
@@ -1,9 +0,0 @@
-class alien:
-    def __init__(self, y, x, life):
-        self.y = y
-        self.x = x
-        self.life = life
-        alien.endtime = time.time() + self.life
-
-    def displayEndtime(self):
-        print("Endtime: ", alien.endtime)
